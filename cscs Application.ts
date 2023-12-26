1e7b7fdfd36283e937e9b9420cd7e9476ad67e10
Ramya_6500 <45486376+Undapalli@users.noreply.github.com>
2023-12-26 14:37:24 +0530

    Add files via upload

---
+++ cscs Application.ts
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>cscs Application</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>20</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>e59ed95a-ebb4-40e0-837a-43ccee6b5ae0</testSuiteGuid>
+   <testCaseLink>
+      <guid>3bb64c37-28c3-4b31-a5f7-e2c533b91812</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CSCS Application</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>44de0f92-b713-4730-b81d-cc92631b0f1c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CSCS</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>b004d4ec-a29f-4067-bb60-d15c017d7a77</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>c7f2fe8c-790d-45ce-84b5-c739937077ce</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
